0afacf8fa75958edbd1b9bc2b980369bcee27a01
Mrloftz <skro3ert@hotmail.com>
2019-08-05 01:24:26 +0700

    set Create Edit validate

--- src/components/createBanner.js
+++ src/components/createBanner.js
@@ -1,23 +1,57 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Formik, Field } from 'formik'
 import { FieldInput, FieldInputDatePicker, FieldCheckboxInput, FieldFileInput, FieldRadioInput } from '../components/input'
 import { Button } from 'antd'
 import { LoginSchema } from '../helper/validator'
 import * as moment from 'moment'
 import styled from 'styled-components'
-import { UploadBanner } from '../_service/bannerMethodApi'
+import { UploadBanner, Getlanguage, GetBanner, DeleteBanner } from '../_service/bannerMethodApi'
+import axios from 'axios'
+import { BreadCrumb } from '../components/breadcrum'
 
-export const BannerComponent = () => {
-  const [status, setStatus] = useState([{ id: 0, label: 'enable', value: 1 }, { id: 1, label: 'disable', value: 0 }])
-  const [langs, setLangs] = useState([{ id: 0, label: 'TH' }, { id: 1, label: 'ENG' }])
-  const [langActive, setActive] = useState('TH')
+export const BannerComponent = props => {
+  const [statusMap, setStatusMap] = useState([{ id: 0, label: 'enable', value: 0 }, { id: 1, label: 'disable', value: 1 }])
+  const [langs, setLangs] = useState([])
+  const [langActive, setActive] = useState('th')
+  const [name, setName] = useState()
+  const [link, setLink] = useState()
+  const [sortOrder, setsortOrder] = useState()
+  const [startdate, setStartDate] = useState(moment())
+  const [enddate, setEndDate] = useState()
+  const [lastUpdate, setlastUpdate] = useState()
+  const [fileenPreview, setimageUrlth] = useState()
+  const [filethPreview, setimageUrlen] = useState()
+  const [status, setStatus] = useState(statusMap[0].value)
 
+  const CheckParams = props.params
+  const { history } = props
+  useEffect(() => {
+    const fetchData = async () => {
+      const langs = await Getlanguage()
+      setLangs(langs.data.data.language)
+    }
+    fetchData()
+    const fetchValue = async id => {
+      const { data } = await GetBanner(id)
+      setName(data.data.banner.name)
+      setLink(data.data.banner.link)
+      setsortOrder(data.data.banner.sortOrder)
+      setStartDate(moment(data.data.banner.startdate))
+      setEndDate(moment(data.data.banner.enddate))
+      setlastUpdate(data.data.banner.lastUpdate)
+      setimageUrlen(data.data.banner.bannerTranslations[0].imageUrl)
+      setimageUrlth(data.data.banner.bannerTranslations[1].imageUrl)
+      setStatus(data.data.banner.status)
+    }
+
+    if (CheckParams.id) {
+      fetchValue(CheckParams.id)
+    }
+  }, [CheckParams.id])
   const fileLang = `file${langActive}`
-
   const handleChangeFile = async (formikProps, file) => {
     let isUploadError = false
     const imageFile = file.currentTarget.files[0]
-
     if (!imageFile) {
       isUploadError = true
       formikProps.setFieldError(fileLang, 'Required')
@@ -28,9 +62,7 @@ export const BannerComponent = () => {
       isUploadError = true
       formikProps.setFieldError(fileLang, 'File size is exceeded')
     }
-
     formikProps.setFieldValue(fileLang, imageFile)
-
     if (!isUploadError) {
       let body = new FormData()
       body.append('bannerImage', imageFile)
@@ -38,8 +70,21 @@ export const BannerComponent = () => {
       try {
         const result = await UploadBanner(body)
         const imageUrl = result.data.data.imageUrl
+        console.log(imageUrl)
         formikProps.setFieldValue(`${fileLang}Preview`, imageUrl)
       } catch (e) {}
+    }
+  }
+
+  const onChangeActiveAllLangs = (formikProps, e) => {
+    const { checked } = e.target
+
+    formikProps.setFieldValue('activeAllLangs', checked)
+
+    if (checked) {
+      setActive('th')
+      formikProps.setFieldValue(`fileen`, '')
+      formikProps.setFieldValue(`fileenPreview`, '')
     }
   }
 
@@ -48,35 +93,108 @@ export const BannerComponent = () => {
       <FormContainer>
         <Formik
           initialValues={{
-            status: status[0].value,
-            startdate: moment(),
+            name,
+            link,
+            sortOrder,
+            status,
+            startdate,
+            enddate,
+            fileenPreview,
+            filethPreview,
+            lastUpdate,
           }}
+          enableReinitialize={true}
           validate={LoginSchema}
           // validationSchema={LoginSchema}
-          onSubmit={values => {
-            console.log(values)
+          onSubmit={async formValues => {
+            if (CheckParams.id) {
+              let value = {
+                id: CheckParams.id,
+                name: formValues.name,
+                link: formValues.link,
+                sortOrder: formValues.sortOrder,
+                startTime: moment(formValues.startdate).format('YYYY-MM-DD'),
+                endTime: moment(formValues.enddate).format('YYYY-MM-DD'),
+                sortOrder: formValues.sortOrder,
+                bannerTranslations: [
+                  {
+                    languageId: 2,
+                    imageUrl: formValues.fileenPreview,
+                  },
+                  {
+                    languageId: 5,
+                    imageUrl: formValues.filethPreview,
+                  },
+                ],
+              }
+              await axios
+                .post('http://travizgo.dosetech.co:7788/banner/update', value)
+                .then(res => {
+                  console.log(res)
+                })
+                .catch(error => {
+                  console.log(error)
+                })
+            } else {
+              let data = {
+                name: formValues.name,
+                status: formValues.status,
+                link: formValues.link,
+                sortOrder: formValues.sortOrder,
+                startTime: moment(formValues.startdate).format('YYYY-MM-DD'),
+                endTime: moment(formValues.enddate).format('YYYY-MM-DD'),
+                sortOrder: formValues.sortOrder,
+                bannerTranslations: [
+                  {
+                    languageId: 2,
+                    imageUrl: formValues.fileenPreview,
+                  },
+                  {
+                    languageId: 5,
+                    imageUrl: formValues.filethPreview,
+                  },
+                ],
+              }
+              await axios
+                .post('http://travizgo.dosetech.co:7788/banner/create', data)
+                .then(response => {
+                  console.log(response)
+                })
+                .catch(error => {
+                  console.log(error)
+                })
+            }
+
+            history.push('/')
           }}
           render={props => (
             <form onSubmit={props.handleSubmit}>
               <LanguageContainer>
+                {!CheckParams.id && <h1>Create Banner</h1>}
+                {CheckParams.id && <h1>Edit Banner</h1>}
+                <BreadCrumb />
                 <Field
-                  name="testing"
+                  name="activeAllLangs"
                   component={FieldCheckboxInput}
                   value={props.values.testing}
-                  onChange={props.handleChange}
+                  onChange={e => onChangeActiveAllLangs(props, e)}
                   checkbox_text={'ใช้กับทุกภาษา'}
                 />
                 <div>
-                  {langs.map((lang, index) => {
+                  {langs.map((langs, index) => {
                     return (
-                      <LanguageButton isActive={langActive === lang.label} onClick={() => setActive(lang.label)} key={index}>
-                        {lang.label}{' '}
+                      <LanguageButton
+                        isActive={langActive === langs.code}
+                        onClick={() => !props.values.activeAllLangs && setActive(langs.code)}
+                        key={index}
+                      >
+                        {langs.code}
+                        {props.errors[`file${langs.code}`] && <div style={{ color: 'red' }}>error</div>}
                       </LanguageButton>
                     )
                   })}
                 </div>
               </LanguageContainer>
-
               <div>Name</div>
               <Field
                 name="name"
@@ -86,13 +204,10 @@ export const BannerComponent = () => {
                 placeholder="Name"
                 format="YYYY-MM-DD"
               />
-
               <div>URL</div>
               <Field name="link" component={FieldInput} value={props.values.link} onChange={props.handleChange} placeholder="Link" />
-
               <div>Sort Order</div>
               <Field name="sortOrder" component={FieldInput} value={props.values.sortOrder} onChange={props.handleChange} placeholder="SortOrder" />
-
               <div>Start Date</div>
               <Field
                 name="startdate"
@@ -101,7 +216,6 @@ export const BannerComponent = () => {
                 onChange={e => props.setFieldValue('startdate', e)}
                 placeholder="Start Date"
               />
-
               <div>End Date</div>
               <Field
                 name="enddate"
@@ -110,22 +224,18 @@ export const BannerComponent = () => {
                 onChange={e => props.setFieldValue('enddate', e)}
                 placeholder="End Date"
               />
-
               <div>File</div>
               <Field name={fileLang} component={FieldFileInput} value={''} onChange={e => handleChangeFile(props, e)} placeholder={fileLang} />
-
               <img alt="" style={{ width: '25%' }} src={props.values[`${fileLang}Preview`]} />
-
               <div>Status</div>
-              <Field name="status" component={FieldRadioInput} value={props.values.status} onChange={props.handleChange} data={status} />
+              <Field name="status" component={FieldRadioInput} value={props.values.status} onChange={props.handleChange} data={statusMap} />
               <ContainerButton>
-                <Button type="danger">Remove</Button>
+                <Button type="danger" onClick={() => DeleteBanner(CheckParams.id)}>
+                  Remove
+                </Button>
                 <Button>Cancel</Button>
                 <Button type="primary" htmlType="submit">
                   Save
-                </Button>
-                <Button type="primary" htmlType="button">
-                  Save & Exit
                 </Button>
               </ContainerButton>
             </form>
