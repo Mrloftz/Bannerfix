d5799d7dd3db8194d59b6f5bbd4fd89f2bbd350a
Mrloftz <skro3ert@hotmail.com>
2019-08-05 01:25:37 +0700

    set props params ID and props history /

--- src/pages/banner.js
+++ src/pages/banner.js
@@ -4,9 +4,12 @@ import { HomeLayout } from '../components/layout/home-layout'
 
 class BannerPage extends React.Component {
   render() {
+    const params = this.props.match.params
+
+    const { history } = this.props
     return (
       <HomeLayout>
-        <BannerComponent />
+        <BannerComponent params={params} history={history} />
       </HomeLayout>
     )
   }
